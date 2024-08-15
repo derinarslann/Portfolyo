08f2ec84e55a1347c88849e4edee6ced4949abff
derinarslann <168091433+derinarslann@users.noreply.github.com>
2024-08-15 18:09:19 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# İçerik sayfasını çalıştırma
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Dinamik beceriler
+@app.route('/', methods=['POST', "GET"])
+def process_form():
+    if request.method == "POST":
+        button_python = request.form.get('button_python')
+        button_discord  = request.form.get('button_discord')
+        button_html = request.form.get('button_html')
+        button_db = request.form.get('button_db')
+        return render_template('index.html', 
+                               button_python=button_python,
+                               button_discord=button_discord,
+                               button_html=button_html,
+                               button_db=button_db)
+    else:
+        return render_template('index.html')
+
+@app.route("/submit", methods=['POST'])
+def submit_comment():
+    email   = request.form.get("email")
+    comment = request.form.get("text")
+    with open(f"{email}.txt", "w") as doc:
+        doc.write(f"{email}\n{comment}")
+    return render_template("index.html")
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
